c57aabeb966aa16ef10a4ddaec3834c7b99a1bdc
Liplesh <Lipnin99@gmail.com>
2024-12-02 21:50:18 +0300

    add SpringSecurity

--- src/main/java/org/example/WebAppInitializer.java
+++ src/main/java/org/example/WebAppInitializer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.apache.log4j.Logger;
+import org.example.app.config.AppContextConfig;
 import org.example.web.config.WebContextConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -19,8 +20,10 @@ public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
 
         log.info("loading app configuration");
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("classpath:app-config.xml");
+//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+//        appContext.setConfigLocation("classpath:app-config.xml");
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(AppContextConfig.class);
         servletContext.addListener(new ContextLoaderListener(appContext));
 
         log.info("loading web configuration");
---
+++ src/main/java/org/example/app/config/AppSecurityConfig.java
@@ -0,0 +1,62 @@
+package org.example.app.config;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    Logger log = Logger.getLogger(AppSecurityConfig.class);
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("config in memory auth user");
+        auth
+                .inMemoryAuthentication()
+                .withUser("root")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER");
+        log.debug("****After config in memory auth");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("config http security");
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/login*").permitAll() // Разрешаем все начинающееся с /login
+                .anyRequest().authenticated() // Все реквесты должны проходить аутентификацию
+                .and()
+                .formLogin()
+                .loginPage("/login") // Предоставляем собственную форму логина
+                .loginProcessingUrl("/login/auth") // Процесс, который обрабатывает аутентификацию
+                .defaultSuccessUrl("/books/shelf", true) // Страница по умолчанию в случае успешного логина
+                .failureUrl("/login"); // Страница в случае ошибки
+        log.debug("****After config http security");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        log.info("config web security");
+        web
+                .ignoring() // игнорировать запросы, которые начинаются с
+                .antMatchers("/images/**");
+    }
+}
